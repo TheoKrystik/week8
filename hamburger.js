0f68b90c3029cec3e975ebf2b2c384a5a6ccb101
TheoK <149838357+TheoKrystik@users.noreply.github.com>
2025-03-04 13:49:21 -0700

    update

--- hamburger.js
+++ hamburger.js
@@ -1,1 +1,6 @@
 const navToggle = document.getElementById('nav-toggle');
+const menuList = document.getElementById('main-menu');
+
+navToggle.addEventListener('click', function(){
+    menuList.classList.toggle('active');
+});
